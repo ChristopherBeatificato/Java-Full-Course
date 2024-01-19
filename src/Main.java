e40589edc4abe162a6b4ed07d1e93b96dd228fec
ChristopherBeatificato <149751224+ChristopherBeatificato@users.noreply.github.com>
2024-01-19 10:29:18 -0800

    Update 1.0
    Notes for:
    Nonprimitive Data Types
    Data Type Conversions
    How to write a method
    Switch Cases
    Two Dimensional Arrays

---
+++ src/Main.java
@@ -0,0 +1,52 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+
+
+/*
+NonPrimitive Data Types
+
+    String str = "test"; [creates a object that can be used multiple times]
+    String str = new String ("test"); [creates a new object when called]
+
+    int arr[]; [creates an int array]
+    arr = new int [2]; [States the storage size of the array]
+
+Data Type Conversions*
+
+    Implicit casting example
+    int a = 100;
+    long b = a; [this is implicit data casting because it's going from a smaller datatype to a larger one]
+
+    Explicit casting
+    double a = 50.50;
+    float b = (float)a; [this is explicit data casting because it's going from a larger datatype to a smaller one.]
+
+Hello World Program
+
+public [entry point method has to be public for java to find it]
+static [whenever you want to access something within the class without making an object]
+void [the return type, void will not return anything]
+main [the name, needs to be main]
+
+Switch Cases
+
+    int month = 6;
+    String monthString
+    switch (month) {
+    case 1: [the 1 represents the variable you want to look at]
+        monthString = "January";
+        break;
+     default: [if none of the case conditions are true, then this will apply]
+        monthString = "Invalid Month";
+        break;
+    }
+
+Two Dimensional Arrays
+    int arr[][]
+
+ */
