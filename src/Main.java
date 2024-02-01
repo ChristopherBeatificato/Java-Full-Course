99b00cd190597de38fe35ed4830c1e4c1f3ae012
ChristopherBeatificato <149751224+ChristopherBeatificato@users.noreply.github.com>
2024-02-01 09:20:45 -0800

    More notes #5

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         int arr[][] = { {2, 7, 9}, {3, 6, 1}, {7, 4, 2} };
         for (int i = 0; i < 3; i++){
             for (int j = 0; j < 3; j++){
-                System.out.println(arr[i][j] + " ");
+                System.out.print(arr[i][j] + " ");
             }
             System.out.println();
         }
@@ -99,7 +99,7 @@ Nested For Loop
     int arr[][] = { {2, 7, 9}, {3, 6, 1}, {7, 4, 2} }
     for (int i = 0; i < 3; i++){
         for (int j = 0; j < 3; j++){
-            System.out.println(arr[i][j] + " ");
+            System.out.print(arr[i][j] + " ");
         }
         System.out.println();
     }
